ea1a7903fe5d7ddba3f28adfa5a64ab713478510
david_meridian <ddelke@meridian-integration.com>
2019-03-06 10:27:53 -0500

    changed package.js

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'dburles:two-factor',
-  version: '1.3.0',
-  summary: 'Two-factor authentication for accounts-password',
-  git: 'https://github.com/dburles/meteor-two-factor.git',
-  documentation: 'README.md',
+   name: 'red62labs:two-factor',
+   version: '1.3.0',
+   summary: 'Two-factor authentication for accounts-password',
+   git: 'https://github.com/red62labs/meteor-two-factor.git',
+   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use(['ecmascript', 'check']);
-  api.use('reactive-dict', 'client');
-  api.use('accounts-password', ['client', 'server']);
-  api.addFiles('common.js');
-  api.addFiles('client.js', 'client');
-  api.addFiles('server.js', 'server');
-  api.export('twoFactor');
+Package.onUse((api) => {
+   api.versionsFrom('1.2.1');
+   api.use(['ecmascript', 'check']);
+   api.use('reactive-dict', 'client');
+   api.use('accounts-password', ['client', 'server']);
+   api.addFiles('common.js');
+   api.addFiles('client.js', 'client');
+   api.addFiles('server.js', 'server');
+   api.export('twoFactor');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('dburles:two-factor');
-  api.addFiles('tests.js');
+Package.onTest((api) => {
+   api.use('ecmascript');
+   api.use('tinytest');
+   api.use('dburles:two-factor');
+   api.addFiles('tests.js');
 });
